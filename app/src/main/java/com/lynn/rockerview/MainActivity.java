9d0b9123b06689de8ed06f133e261b123224c093
Lynn <f53535350@gmail.com>
2018-04-02 09:55:22 +0800

    2018/04/02 09:54

--- app/src/main/java/com/lynn/rockerview/MainActivity.java
+++ app/src/main/java/com/lynn/rockerview/MainActivity.java
@@ -3,6 +3,10 @@ package com.lynn.rockerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTvLevel;
     private TextView mTvModel;
     private LinearLayout linear;
+    private LinearLayout linear2;
 
 
     @Override
@@ -30,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
 //                LinearLayout.LayoutParams.MATCH_PARENT);
 
 
-
-        mTvModel.setText("当前模式：方向有改变时回调；8个方向");
+       // mTvModel.setText("当前模式：方向有改变时回调；8个方向");
 
         mRockerView.setOnShakeListener(DIRECTION_8, new RockerView.OnShakeListener() {
             @Override
@@ -42,23 +46,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void direction(RockerView.Direction direction) {
                 if (direction == RockerView.Direction.DIRECTION_CENTER) {
-                    mTvShake.setText("当前方向：中心");
+                   // mTvShake.setText("当前方向：中心");
                 } else if (direction == RockerView.Direction.DIRECTION_DOWN) {
-                    mTvShake.setText("当前方向：下");
+                   // mTvShake.setText("当前方向：下");
                 } else if (direction == RockerView.Direction.DIRECTION_LEFT) {
-                    mTvShake.setText("当前方向：左");
+                 //   mTvShake.setText("当前方向：左");
                 } else if (direction == RockerView.Direction.DIRECTION_UP) {
-                    mTvShake.setText("当前方向：上");
+                  //  mTvShake.setText("当前方向：上");
                 } else if (direction == RockerView.Direction.DIRECTION_RIGHT) {
-                    mTvShake.setText("当前方向：右");
+                  //  mTvShake.setText("当前方向：右");
                 } else if (direction == RockerView.Direction.DIRECTION_DOWN_LEFT) {
-                    mTvShake.setText("当前方向：左下");
+                  //  mTvShake.setText("当前方向：左下");
                 } else if (direction == RockerView.Direction.DIRECTION_DOWN_RIGHT) {
-                    mTvShake.setText("当前方向：右下");
+                  //  mTvShake.setText("当前方向：右下");
                 } else if (direction == RockerView.Direction.DIRECTION_UP_LEFT) {
-                    mTvShake.setText("当前方向：左上");
+                   // mTvShake.setText("当前方向：左上");
                 } else if (direction == RockerView.Direction.DIRECTION_UP_RIGHT) {
-                    mTvShake.setText("当前方向：右上");
+                  //  mTvShake.setText("当前方向：右上");
                 }
             }
 
@@ -75,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void angle(double angle) {
-                mTvAngle.setText("当前角度：" + angle);
+                //mTvAngle.setText("当前角度：" + angle);
             }
 
             @Override
@@ -87,18 +91,22 @@ public class MainActivity extends AppCompatActivity {
         mRockerView.setOnDistanceLevelListener(new RockerView.OnDistanceLevelListener() {
             @Override
             public void onDistanceLevel(int level) {
-                mTvLevel.setText("当前距离级别：" + level);
+                //mTvLevel.setText("当前距离级别：" + level);
             }
         });
     }
 
     private void findView() {
-
+        linear = (LinearLayout) findViewById(R.id.linear);
         mRockerView = (RockerView) findViewById(R.id.my_rocker);
-        mTvAngle = (TextView) findViewById(R.id.tv_now_angle);
-        mTvLevel = (TextView) findViewById(R.id.tv_now_level);
-        mTvModel = (TextView) findViewById(R.id.tv_now_model);
-        mTvShake = (TextView) findViewById(R.id.tv_now_shake);
+//        mTvAngle = (TextView) findViewById(R.id.tv_now_angle);
+//        mTvLevel = (TextView) findViewById(R.id.tv_now_level);
+//        mTvModel = (TextView) findViewById(R.id.tv_now_model);
+//        mTvShake = (TextView) findViewById(R.id.tv_now_shake);
+        //Button btn_test = (Button)findViewById(R.id.btn_test);
+
+        DragViewUtil.drag(mRockerView);
     }
+
 
 }
